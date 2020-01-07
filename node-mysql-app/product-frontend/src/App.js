51136711eb0dcd3399418a9b53432819ca14290e
sincerity628 <14785481094@163.com>
2020-01-07 22:53:12 +0800

    finished ndoe & mysql & react product app.

--- node-mysql-app/product-frontend/src/App.js
+++ node-mysql-app/product-frontend/src/App.js
@@ -4,28 +4,55 @@ import axios from 'axios';
 const base_url = 'http://localhost:4000/products';
 const initProduct = {
   productName: '',
-  price: ''
+  price: 0
 };
 
 function App() {
   const [products, setProducts] = useState([]);
   const [product, setProduct] = useState(initProduct);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
+    let unmounted = false;
     axios
       .get(base_url)
       .then(res => {
-        if(res.status === 200) {
+        if(!unmounted && res.status === 200) {
           setProducts(res.data);
-          console.log(res);
         }
       })
       .catch(error => {
         console.log(error);
       })
-  }, []);
 
-  console.log(products);
+    return () => unmounted = true;
+  }, [refresh]);
+
+  const handleChange = e => {
+    setProduct({
+      ...product,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .get(base_url + '/add', { params: {
+        productName: product.productName,
+        price: product.price
+      } })
+      .then(res => {
+        if(res.status === 200) {
+          setRefresh(!refresh);
+          setProduct(initProduct);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
   return (
     <div className="App">
       <h1>production</h1>
@@ -36,6 +63,25 @@ function App() {
           </p>
         )) }
       </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="productName">product name: </label>
+        <input
+          type="text"
+          id="productName"
+          value={product.productName}
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="price">price:</label>
+        <input
+          type="number"
+          id="price"
+          value={product.price}
+          onChange={handleChange}
+        />
+        <br />
+        <button>add to products</button>
+      </form>
     </div>
   );
 }
