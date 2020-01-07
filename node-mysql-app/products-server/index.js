3b46fad6823bcbed779b1a9d6ce96f5a48b7e953
sincerity628 <14785481094@163.com>
2020-01-07 20:38:27 +0800

    connected mysql.

--- node-mysql-app/products-server/index.js
+++ node-mysql-app/products-server/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 app.use(cors());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '******',
+  database: 'production'
+});
+
+connection.connect(error => {
+  if(error) {
+    console.log(error);
+    return error;
+  }
+  console.log('Connected!');
+});
 
 app.get('/', (req, res) => {
   res.send('hello from the product server.');
