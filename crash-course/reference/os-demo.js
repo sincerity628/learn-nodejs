54410cc6147de648734c2c576e75add2741f10ec
sincerity628 <14785481094@163.com>
2020-01-11 23:42:31 +0800

    operating system module.

---
+++ crash-course/reference/os-demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// platform
+console.log(os.platform());
+
+// CPU architecture
+console.log(os.arch());
+
+// CPU core info
+console.log(os.cpus());
+
+// free memory
+console.log(os.freemem());
+
+// total memory
+console.log(os.totalmem());
+
+// home dir
+console.log(os.homedir());
+
+// up time
+console.log(os.uptime());
