a5180fea894d6af881ae92a1ca846bcaab53e093
sincerity628 <14785481094@163.com>
2020-01-12 18:05:28 +0800

    added event module.

---
+++ crash-course/reference/event-demo.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+// create the class
+class MyEmitter extends EventEmitter {}
+// init object
+const myEmitter = new MyEmitter();
+// event listener
+myEmitter.on('event', () => console.log('event fired.'));
+// init event
+myEmitter.emit('event'); 
